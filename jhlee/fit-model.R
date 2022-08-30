fb5d46b7b45407099dac14a2bead23f8343bf7a0
JeunghyunLee <jhleeangel@gmail.com>
2022-08-30 23:23:10 +0900

    update

--- jhlee/fit-model.R
+++ jhlee/fit-model.R
@@ -2,7 +2,6 @@ library(rstan)
 library(ggbump)
 library(ggplot2)
 library(tidyverse)
-source("~/Github/project_stressSD/bayesian_modeling/utils/M16_simulator_figure.R")
 
 options(mc.cores =4)
 
@@ -12,7 +11,7 @@ FOLDER_STAN = sprintf('%smodels/', FOLDER_MODEL)
 FOLDER_OUTPUT = sprintf('%sstan-output/', FOLDER_MODEL)
 MODEL_OUTPUT = sprintf('%smodel-output/', FOLDER_MODEL)
 
-NAME_MODEL = "M15"
+NAME_MODEL = "M18"
 
 FILE_DATA = sprintf("%sdata/SD_120_whole_n41_fairSeq.txt", FOLDER_MODEL)
 FILE_MODEL = sprintf('%s%s.stan', FOLDER_STAN, NAME_MODEL)
@@ -60,7 +59,6 @@ data <- read.table(FILE_DATA, sep = '\t', header = T)
   psplit           = array(0, c(nump,numTrial))
 } 
 
-print(nump)
 #assigning conditions //
 for (i in 1:nump){
   tmpData = subset(data, subjID == 1) #use the first subject data for conditons. 
@@ -86,98 +84,98 @@ for (i in 1:nump){
 
 
 #fakedata input dataList
-fdList <- list(
-  N               = nump,
-  subjID          = subjID,
-  amount_self     = amount_self,
-  amount_other    = amount_other,
-  amount_default  = amount_default,
-  social_distance = social_distance,
-  inequality      = amount_other - amount_default
-)
-
-params = array(0, c(nump, 4))
-counts = 1
-for (k in 1:length(K)){
-  for (beta in 1:length(BETA)) {
-    for (tau in 1:length(TAU)) {
-      for (eta in 1:length(ETA)) {
-        # generate the fake data for 10 subjects at given parameters.
-        # EVO[counts,] = M16_simulator_SVnet(dataList = fdList,
-        #                                    k = K[k],
-        #                                    beta = BETA[beta],
-        #                                    tau = TAU[tau],
-        #                                    eta = ETA[eta],
-        #                                    nrep = 1)
-        psplit[counts,] = M16_simulator_psplit(dataList = fdList,
-                                         k = K[k],
-                                         beta = BETA[beta],
-                                         tau = TAU[tau],
-                                         eta = ETA[eta],
-                                         nrep = 1)
-        params[counts, 1] = K[k]
-        params[counts, 2] = BETA[beta]
-        params[counts, 3] = TAU[tau]
-        params[counts, 4] = ETA[eta]
-        counts = counts + 1
-      }
-    }
-  }
-}
-
-df_params <- as_tibble(params)
-colnames(df_params) <- c("k", "beta", "tau", "eta")
-
-# p(split) = 1/(1+exp(-tau*(EVO-EVS))
-### plot ### 
-sigmoid <- function(tau, x){
-  y = 1/(1+exp(-tau*(x)))
-}
-dataList %>% 
-  # group_by(as.numeric(unlist(social_distance)), 
-  #          as.numeric(unlist(amount_self)),
-  #          as.numeric(unlist(amount_other))) %>% 
-  summarize(
-    x = df_params$tau,
-    y = sigmoid(x, EVO, EVS)
-  ) %>% 
-  ggplot(aes(x = x, y = y)) + 
-  geom_line()
-
-### ref ### 
-f <- function(x, b0,b1,b2,b3) b0*exp(-0.5*((x-b1)/b2)^2) + b3
-
-df.func <- df %>% 
-  group_by(group, b0, b1, b2, b3) %>% 
-  summarize(
-    x = seq(0, 20, length = 100),
-    y = f(x, b0, b1, b2, b3)
-  ) 
-df.points <- df.func %>% 
-  sample_n(10)
-
-ggplot(df.func, aes(x = x, y = y, color = group))+ 
-  geom_line() +
-  geom_point(data = df.points)
-### end of example 
-
-# simple sigmoid 
-p <- ggplot(data = data.frame(x = c(1, 5, 10, 20, 50, 100)), aes(x))
-p + stat_function(fun = sigmoid, args = c(1, 10), n = 100) 
+# fdList <- list(
+#   N               = nump,
+#   subjID          = subjID,
+#   amount_self     = amount_self,
+#   amount_other    = amount_other,
+#   amount_default  = amount_default,
+#   social_distance = social_distance,
+#   inequality      = amount_other - amount_default
+# )
 # 
-ggplot(test,aes(x=t, y=fold))+ 
-  #to make it obvious I use argument names instead of positional matching
-  geom_point()+
-  geom_smooth(method="nls", 
-              formula=y~1+Vmax*(1-exp(-x/tau)), # this is an nls argument, 
-              #but stat_smooth passes the parameter along
-              start=c(tau=0.2,Vmax=2), # this too
-              se=FALSE) # this is an argument to stat_smooth and 
-# switches off drawing confidence intervals
+# params = array(0, c(nump, 4))
+# counts = 1
+# for (k in 1:length(K)){
+#   for (beta in 1:length(BETA)) {
+#     for (tau in 1:length(TAU)) {
+#       for (eta in 1:length(ETA)) {
+#         # generate the fake data for 10 subjects at given parameters.
+#         # EVO[counts,] = M16_simulator_SVnet(dataList = fdList,
+#         #                                    k = K[k],
+#         #                                    beta = BETA[beta],
+#         #                                    tau = TAU[tau],
+#         #                                    eta = ETA[eta],
+#         #                                    nrep = 1)
+#         psplit[counts,] = M16_simulator_psplit(dataList = fdList,
+#                                          k = K[k],
+#                                          beta = BETA[beta],
+#                                          tau = TAU[tau],
+#                                          eta = ETA[eta],
+#                                          nrep = 1)
+#         params[counts, 1] = K[k]
+#         params[counts, 2] = BETA[beta]
+#         params[counts, 3] = TAU[tau]
+#         params[counts, 4] = ETA[eta]
+#         counts = counts + 1
+#       }
+#     }
+#   }
+# }
 # 
-
-print(rowSums(split)) #This summates # of prosocial decisions
-print(params)
+# df_params <- as_tibble(params)
+# colnames(df_params) <- c("k", "beta", "tau", "eta")
+# 
+# # p(split) = 1/(1+exp(-tau*(EVO-EVS))
+# ### plot ### 
+# sigmoid <- function(tau, x){
+#   y = 1/(1+exp(-tau*(x)))
+# }
+# dataList %>% 
+#   # group_by(as.numeric(unlist(social_distance)), 
+#   #          as.numeric(unlist(amount_self)),
+#   #          as.numeric(unlist(amount_other))) %>% 
+#   summarize(
+#     x = df_params$tau,
+#     y = sigmoid(x, EVO, EVS)
+#   ) %>% 
+#   ggplot(aes(x = x, y = y)) + 
+#   geom_line()
+# 
+# ### ref ### 
+# f <- function(x, b0,b1,b2,b3) b0*exp(-0.5*((x-b1)/b2)^2) + b3
+# 
+# df.func <- df %>% 
+#   group_by(group, b0, b1, b2, b3) %>% 
+#   summarize(
+#     x = seq(0, 20, length = 100),
+#     y = f(x, b0, b1, b2, b3)
+#   ) 
+# df.points <- df.func %>% 
+#   sample_n(10)
+# 
+# ggplot(df.func, aes(x = x, y = y, color = group))+ 
+#   geom_line() +
+#   geom_point(data = df.points)
+# ### end of example 
+# 
+# # simple sigmoid 
+# p <- ggplot(data = data.frame(x = c(1, 5, 10, 20, 50, 100)), aes(x))
+# p + stat_function(fun = sigmoid, args = c(1, 10), n = 100) 
+# # 
+# ggplot(test,aes(x=t, y=fold))+ 
+#   #to make it obvious I use argument names instead of positional matching
+#   geom_point()+
+#   geom_smooth(method="nls", 
+#               formula=y~1+Vmax*(1-exp(-x/tau)), # this is an nls argument, 
+#               #but stat_smooth passes the parameter along
+#               start=c(tau=0.2,Vmax=2), # this too
+#               se=FALSE) # this is an argument to stat_smooth and 
+# # switches off drawing confidence intervals
+# # 
+# 
+# print(rowSums(split)) #This summates # of prosocial decisions
+# print(params)
 
 Tsubj = rep(numTrial,nump)
 dataList <- list(
